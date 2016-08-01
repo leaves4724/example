74c30daaa437fb99bbe37d8bcedb12fa0838541a
leaves4724 <1143834945@qq.com>
2016-08-01 19:50:05 +0800

    Create average.php

---
+++ average.php
@@ -0,0 +1,18 @@
+<?php
+$fp=fopen("d:/PhpstormProjects/data/log.txt","r");
+$arrSum=[];
+$arrNum=[];
+$aver=array();
+while(feof($fp)!=true){
+    $row=trim(fgets($fp));
+    $arr=explode(" ",$row);
+    if(!isset($arrSum[$arr[1]])) {
+        $arrSum[$arr[1]] = 0;
+    }
+    $arrSum[$arr[1]] += $arr[2];
+
+    $arrNum[$arr[1]]++;
+}
+foreach ($arrSum as $k=>$v) {
+    echo $k .":".($v/$arrNum[$k])."\n";
+}
